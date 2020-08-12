5d83e91ee7c8efa224962ebad2c643e479541fb4
Wandering Robot <ej_braun@yahoo.com>
2020-08-12 14:53:54 -0700

    got the bare gui for pre-program navigation

--- gui.py
+++ gui.py
@@ -1,75 +1,96 @@
 from tkinter import *
+import tkinter as tk
 
-class Tinker:
+class Page(Frame):
+    def __init__(self,*args,**kwargs):
+        Frame.__init__(self,*args,**kwargs)
+
+    def show(self):
+        self.lift()
+
+class MainPage(Page):
+    def __init__(self,gui,*args,**kwargs):
+        Page.__init__(self,*args,**kwargs)
+
+        self.gui = gui
+        main_label1 = Label(self,text='Main Page')
+        main_label1.grid(row=0, column=1)
+
+        new_button = Button(self,text='New',command= lambda: self.gui.show_page('n'))
+        new_button.grid(row=1, column=0)
+
+        new_button = Button(self,text='Retrain',command= lambda: self.gui.show_page('r'))
+        new_button.grid(row=1, column=1)
+
+        new_button = Button(self,text='Display',command= lambda: self.gui.show_page('d'))
+        new_button.grid(row=1, column=2)
+
+class NewPage(Page):
+    def __init__(self,gui,*args,**kwargs):
+        Page.__init__(self,*args,**kwargs)
+
+        self.gui = gui
+        main_label1 = Label(self,text='New Page')
+        main_label1.grid(row=0, column=0)
+
+        main_button = Button(self,text='Main',command= lambda: self.gui.show_page('m'))
+        main_button.grid(row=1, column=0)
+
+class RetrainPage(Page):
+    def __init__(self,gui,*args,**kwargs):
+        Page.__init__(self,*args,**kwargs)
+
+        self.gui = gui
+        main_label1 = Label(self,text='Retrain Page')
+        main_label1.grid(row=0, column=0)
+
+        main_button = Button(self,text='Main',command= lambda: self.gui.show_page('m'))
+        main_button.grid(row=1, column=0)
+
+
+class DisplayPage(Page):
+    def __init__(self,gui,*args,**kwargs):
+        Page.__init__(self,*args,**kwargs)
+
+        self.gui = gui
+        main_label1 = Label(self,text='Display Page')
+        main_label1.grid(row=0, column=0)
+
+        main_button = Button(self,text='Main',command= lambda: self.gui.show_page('m'))
+        main_button.grid(row=1, column=0)
+
+
+class GUI:
     def __init__(self):
         self.root = Tk()
         self.root.title('Ai_Simulator')
+        #self.root.iconbitmap(path to icon)
         self.root.geometry('400x350') 
 
-        self.decided = False
-        while not self.decided:
+        self.main = MainPage(self, padx=125, pady=125)
+        self.new = NewPage(self, padx=125, pady=125)
+        self.retrain = RetrainPage(self, padx=125, pady=125)
+        self.display = DisplayPage(self, padx=125, pady=125)
 
-            choice = self.main_tinker()
-            
-            if choice == 1:
-                self.new_tinker()
-            elif choice == 2:
-                self.retrain_tinker()
-            elif choice == 3:
-                self.display_tinker()
+        self.new.place(x=0, y=0)
+        self.retrain.place(x=0, y=0)
+        self.display.place(x=0, y=0)
+        self.main.place(x=0, y=0)
 
-    def main_tinker(self):
-        self.frame = LabelFrame(self.root,bg='grey',text="Main Screen",padx=90,pady=90)
-        self.frame.pack(padx=10,pady=10)
 
-        lbl = Label(self.frame,text='Choose from the following options',bg='grey')
-        lbl.pack()
-
-        self.radio_var = IntVar()
-        
-        R1 = Radiobutton(self.frame,text='Create New Maze',variable=self.radio_var,value=1,bg='grey')
-        R1.pack()
-        R2 = Radiobutton(self.frame,text='Display an Old AI',variable=self.radio_var,value=2,bg='grey')
-        R2.pack()
-        R3 = Radiobutton(self.frame,text='Retrain an Old AI',variable = self.radio_var,value=3,bg='grey')
-        R3.pack()
-
-        b = Button(self.frame,text='Next',command=self.main_get_next)
-        b.pack()
+        self.main.show()
 
         self.root.mainloop()
 
-    def main_get_next(self):
-        choice = self.radio_var.get()
-        if choice == 1 or choice == 2 or choice == 3:
-            self.frame.destroy()
-        return choice
-
-
-    def new_tinker(self):
-        self.frame = LabelFrame(self.root,bg='grey',text="New Maze",padx=90,pady=90)
-        self.frame.pack(padx=10,pady=10)
-
-        lbl = Label(self.frame,text='Choose from the following options',bg='grey')
-        lbl.pack()
-        self.root.mainloop()
-
-
-
-
-    def retrain_tinker(self):
-        lbl = Label(root,text='retrain')
-        self.root.mainloop()
-
-
-
-
-    def display_tinker(self):
-        lbl = Label(root,text='display')
-        self.root.mainloop()
-
-
-
+    def show_page(self,var):
+        if var == 'n':
+            self.new.show()
+        elif var == 'm':
+            self.main.show()
+        elif var == 'r':
+            self.retrain.show()
+        elif var == 'd':
+            self.display.show()
 
 if __name__ == '__main__':
-    tink = Tinker()
+    gui = GUI()

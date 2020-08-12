5c0e060faeb9859deec09d4c8b742afe8b2803a9
Wandering Robot <ej_braun@yahoo.com>
2020-08-12 16:11:54 -0700

    finished the main menu for the gui

--- gui.py
+++ gui.py
@@ -1,12 +1,15 @@
 from tkinter import *
-import tkinter as tk
 
 class Page(Frame):
     def __init__(self,*args,**kwargs):
         Frame.__init__(self,*args,**kwargs)
 
     def show(self):
+        self.place(x=0, y=0)
         self.lift()
+
+    def hide(self):
+        self.place(x=500,y=500)
 
 class MainPage(Page):
     def __init__(self,gui,*args,**kwargs):
@@ -16,14 +19,39 @@ class MainPage(Page):
         main_label1 = Label(self,text='Main Page')
         main_label1.grid(row=0, column=1)
 
-        new_button = Button(self,text='New',command= lambda: self.gui.show_page('n'))
-        new_button.grid(row=1, column=0)
+        #make Radio Buttons
+        MODES = [
+        ("New", "n"),
+        ("Retrain", "r"),
+        ("Display", "d"),
+        ]
 
-        new_button = Button(self,text='Retrain',command= lambda: self.gui.show_page('r'))
-        new_button.grid(row=1, column=1)
+        self.var = StringVar()
+        self.var.set('n')
+        i=1
+        for text, mode in MODES:
+            b = Radiobutton(self, text=text,variable=self.var, value=mode, command= lambda: self.describe(self.var.get()), justify=LEFT,anchor='nw')
+            b.grid(sticky=W, row=i, column=0)
+            i += 1
 
-        new_button = Button(self,text='Display',command= lambda: self.gui.show_page('d'))
-        new_button.grid(row=1, column=2)
+        #make initial text block
+        self.describe(self.var.get())
+
+        #make continue Button
+        cont = Button(self, text='Continue', command= lambda:self.gui.show_page(self.var.get()))
+        cont.grid(row=4, column=2)
+
+    def describe(self,mode):
+        if mode == 'n':
+            func_text = 'Create a new maze, and train a new AI to solve  it.'
+        elif mode == 'r':
+            func_text = 'Load an old maze from memory, and train a new AI to solve it.'
+        elif mode == 'd':
+            func_text = 'Load an old maze from memory, and display the old AI\'s solution to it.'
+        textbox = Text(self, height=6, width=25, wrap=WORD)
+        textbox.insert('1.0',func_text)
+        textbox.grid(sticky=W, row=1, column=2,rowspan=3)
+
 
 class NewPage(Page):
     def __init__(self,gui,*args,**kwargs):
@@ -67,7 +95,7 @@ class GUI:
         #self.root.iconbitmap(path to icon)
         self.root.geometry('400x350') 
 
-        self.main = MainPage(self, padx=125, pady=125)
+        self.main = MainPage(self, padx=50, pady=100)
         self.new = NewPage(self, padx=125, pady=125)
         self.retrain = RetrainPage(self, padx=125, pady=125)
         self.display = DisplayPage(self, padx=125, pady=125)
@@ -77,12 +105,15 @@ class GUI:
         self.display.place(x=0, y=0)
         self.main.place(x=0, y=0)
 
+        self.pages = [self.main, self.new, self.retrain, self.display]
 
+        self.hide_pages()
         self.main.show()
 
         self.root.mainloop()
 
     def show_page(self,var):
+        self.hide_pages()
         if var == 'n':
             self.new.show()
         elif var == 'm':
@@ -91,6 +122,10 @@ class GUI:
             self.retrain.show()
         elif var == 'd':
             self.display.show()
+
+    def hide_pages(self):
+        for page in self.pages:
+            page.hide()
 
 if __name__ == '__main__':
     gui = GUI()
